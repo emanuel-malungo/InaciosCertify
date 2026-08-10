5d4e858439b3e52dd1cdc1b8d231c558ab8baa70
emanuel-malungo <emanuelmalungo856@gmail.com>
2026-08-10 10:59:55 +0100

    feat: implement landing page layout with header component and static assets

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,10 @@
-import Image from "next/image";
+import Header from "@/components/layout/Header";
+
 
 export default function Home() {
   return (
     <div className="">
+        <Header />
         <h1>EKAKAKK</h1>
     </div>
   );
---
+++ src/components/layout/Header.tsx
@@ -0,0 +1,37 @@
+import Image from "next/image"
+import logo from "@/assets/images/logo.png"
+
+export default function Header() {
+    return (
+        <header className="">
+            <div className="mx-auto container flex items-center justify-between py-4 px-6 lg:px-0">
+                <div>
+                    <Image src={logo} alt="EKANDA" width={100} height={200} />
+                </div>
+
+                <ul className="flex items-center space-x-8" >
+                    <li>
+                        <a href="#">Home</a>
+                    </li>
+                     <li>
+                        <a href="#">Sobre Nós</a>
+                    </li>
+                    <li>
+                        <a href="#">Os nossos serviços</a>
+                    </li>
+                    <li>
+                        <a href="#">Localização</a>
+                    </li>
+                    <li>
+                        <a href="#"><button>Contacte-nos</button></a>
+                    </li>
+                </ul>
+
+                <button  className="bg-primary text-white p-2 round-sm " >
+                    Emitir Certificado
+                </button>
+
+            </div>
+        </header>
+    )
+}
